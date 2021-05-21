2bbabc165b1931099abefbe7765650c0319148ab
ANAGH <anaghkrkkl@gmail.com>
2021-05-21 10:25:18 +0530

    header,banner,rowpost added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
 function App() {
   return (
-    <h1>helloworld</h1>
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <RowPost />
+    </div>
   );
 }
 
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="content">
+                <h1 className="title">Movie Name</h1>
+                <div className="banner_buttons">
+                    <button className="button">Play</button>
+                    <button className="button">My List</button>
+                </div>
+                <h1 className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex omnis cumque recusandae repellat voluptatibus laborum nam culpa. Quasi consequuntur, cupiditate error ut similique nemo quod reiciendis maxime in blanditiis excepturi.</h1>
+            </div>
+            <div className="fade_bottom"></div>
+        </div>
+    )
+}
+
+export default Banner
