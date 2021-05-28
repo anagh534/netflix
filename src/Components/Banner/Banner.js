71f0d532da92868d661c28c96e236cbd5bc6c3b2
ANAGH <anaghkrkkl@gmail.com>
2021-05-28 11:55:08 +0530

    some updates on style

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -7,8 +7,8 @@ function Banner() {
             <div className="content">
                 <h1 className="title">Movie Name</h1>
                 <div className="banner_buttons">
-                    <button className="button">Play</button>
-                    <button className="button">My List</button>
+                    <button className="button"><i class="bi bi-play"></i> Play</button>
+                    <button className="button"><i class="bi bi-plus"></i> My List</button>
                 </div>
                 <h1 className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex omnis cumque recusandae repellat voluptatibus laborum nam culpa. Quasi consequuntur, cupiditate error ut similique nemo quod reiciendis maxime in blanditiis excepturi.</h1>
             </div>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,7 +5,18 @@ function navbar() {
     return (
         <div className="navbar">
             <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix" />
-            <img className="avatar" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
+            <div className="nav-title">
+                <a className="home" href="#">Home</a>
+                <a href="#" className="tvShows">TV Shows</a>
+                <a href="#" className="movies">Movies</a>
+                <a href="#" className="recently-added">Recently Added</a>
+                <a href="#" className="myList">My List</a>
+            </div>
+            <div className="icon">
+                <a href="#" className="searchBtn"><i class="bi bi-search"></i></a>
+                <a href="#"><i class="bi bi-bell"></i></a>
+            </div>
+            <img className="avatar" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
         </div>
     )
 }
