66e38517269d53491ada284569cef03fa3395b8c
anagh534 <108913994+anagh534@users.noreply.github.com>
2022-07-08 10:42:31 +0530

    Updates

---
+++ serviceworker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "netflix-clone";
+const urlsToCache = ['index.html','offline.html'];
+
+const self = this;
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log("install cache");
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+self.addEventListener('fetch', event => {
+    console.log("fetch cache");
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+    console.log("active");
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
